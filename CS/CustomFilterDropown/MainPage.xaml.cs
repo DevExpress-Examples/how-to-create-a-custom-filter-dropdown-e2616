7084c39c945ef4570dfa6b0913167537b8971d4c
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/CustomFilterDropown/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Markup;
+using DevExpress.Data.Filtering;
+
+namespace CustomFilterDropown {
+    public partial class MainPage : UserControl {
+        public MainPage() {
+            InitializeComponent();
+            grid.ItemsSource = GridData.GetData();
+        }
+    }
+
+    public class GridData {
+        static public List<DataObject> GetData() {
+            List<DataObject> data = new List<DataObject>();
+            for (int i = 0; i < 100; i++)
+                data.Add(new DataObject() { Index = i });
+            return data;
+        }
+    }
+    public class DataObject {
+        public int Index { get; set; }
+    }
+    public class IntToCriteriaOperatorConverter : MarkupExtension, IValueConverter {
+        public override object ProvideValue(IServiceProvider serviceProvider) {
+            return this;
+        }
+
+        #region IValueConverter Members
+
+        object IValueConverter.Convert(object value, Type targetType,
+        object parameter, System.Globalization.CultureInfo culture) {
+            BinaryOperator op = value as BinaryOperator;
+            if (object.ReferenceEquals(op, null))
+                return null;
+            OperandValue operandValue = op.RightOperand as OperandValue;
+            return operandValue.Value;
+        }
+        object IValueConverter.ConvertBack(object value, Type targetType,
+                object parameter, System.Globalization.CultureInfo culture) {
+            return new BinaryOperator("Index", Convert.ToInt32(value), BinaryOperatorType.Greater);
+        }
+
+        #endregion
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,10 @@
+# How to Create a Custom Filter Dropdown
+
+
+<p>This example demonstrates how to create a custom filter dropdown for the 'Index' column. To implement the required task, do the following:<br />
+1) Set the column's <i>FilterPopupMode</i> property to <i>FilterPopupM</i><i>ode</i><i>.</i><i>Custom</i><br />
+2) Create a template representing the required filter dropdown and assign it to the <i>CustomColumnFilterPopupTemplate</i> property.</p>
+
+<br/>
+
+
